33269d7ad3068de1ac5ef70f860e18d61807c047
pzeise <phillip.zeise@gmail.com>
2022-07-19 19:08:13 -0400

    got gears working

--- src/Components/PuzzleList.js
+++ src/Components/PuzzleList.js
@@ -6,14 +6,19 @@ import { Link } from 'react-router-dom'
 
 const PuzzleList = () => {
     const {user, puzzles} = useContext(UserContext)
-    const [spin, setSpin] = useState(false)
+    const [spin, setSpin] = useState({})
 
     const setSpinOn = (e) => {
-        setSpin(true);
+        console.log(e._targetInst.key)
+        let key = e._targetInst.key
+        let update = {}
+        update[key] = true
+        console.log(update)
+        setSpin(update);
       };
     
       const setSpinOff = (e) => {
-        setSpin(false);
+        setSpin({});
       };
 
     return (
@@ -21,20 +26,6 @@ const PuzzleList = () => {
             {puzzles.map((puzzle, idx) => (
                 <Link to={`/play/${puzzle._id}`} key={idx} style={{textDecoration: 'none'}}>
                     <div className={styles.card}>
-                        {/* {puzzle.owner.some(id => id === user._id) 
-                        ? <Link to={`/add/${puzzle._id}`} style={{textDecoration: 'none'}}>
-                            <div
-                                className={spin ? styles.editGearSpin : styles.editGear}
-                                onMouseEnter={setSpinOn}
-                                onMouseLeave={setSpinOff}
-                            >
-                                <img
-                                    className={styles.editGearActual}
-                                    src="/gear.svg"
-                                    alt="Add Marker"
-                                ></img>
-                            </div>
-                        </Link> : null} */}
                         <div className={styles.cardImage}>
                             <img className={styles.cardImageActual} 
                                  src={puzzle.show ? puzzle.endState.image : '/globe.png'} 
@@ -44,6 +35,22 @@ const PuzzleList = () => {
                             <h3>{puzzle.show ? puzzle.endState.title : 'Mystery Location'}</h3>
                             {puzzle.show ? <h4>{`Found in ${puzzle.hints}` + (puzzle.hints > 1? ' hints' : ' hint')}</h4> : null}
                         </div>
+                        {puzzle.owner.some(id => id === user._id) 
+                        ? <Link to={`/add/${puzzle._id}`} style={{textDecoration: 'none'}}>
+                            <div
+                                className={spin[idx] ? styles.editGearSpin : styles.editGear}
+                                onMouseEnter={setSpinOn}
+                                onMouseLeave={setSpinOff}
+                                key={idx}
+                            >
+                                <img
+                                    className={styles.editGearActual}
+                                    src="/gear.svg"
+                                    alt="Add Marker"
+                                    key={idx}
+                                ></img>
+                            </div>
+                        </Link> : null}
                     </div>
                 </Link>
             ))}
